b0ec296db541422d4e10669fbc3925e514a6ba10
JoaoSilva <jplimadev@gmail.com>
2026-02-24 21:22:43 -0300

    first commit

---
+++ StudioGlibliApp/StudioGlibliApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudioGlibliApp
+//
+//  Created by Joao Paulo Lima Silva on 24/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudioGlibliApp/StudioGlibliApp/StudioGlibliAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudioGlibliAppApp.swift
+//  StudioGlibliApp
+//
+//  Created by Joao Paulo Lima Silva on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct StudioGlibliAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
